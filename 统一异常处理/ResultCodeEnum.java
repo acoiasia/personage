a99ff9653a52e4bc51ca1a10fe7bedc7b28f98f8
acoiasia <105906390+acoiasia@users.noreply.github.com>
2022-07-02 12:32:59 +0800

    Update ResultCodeEnum.java

--- 统一异常处理/ResultCodeEnum.java
+++ 统一异常处理/ResultCodeEnum.java
@@ -7,9 +7,12 @@ import lombok.Getter;
  */
 @Getter
 public enum ResultCodeEnum {
-    SUCCESS(true,20000,"成功"),
-    UNKNOWN_ERROR(false,20001,"未知错误"),
-    PARAM_ERROR(false,20002,"参数错误"),
+    SUCCESS(true, 20000, "成功"),
+    UNKNOWN_ERROR(false, 20001, "未知错误"),
+    PARAM_ERROR(false, 20002, "参数错误"),
+    NULL_POINT(false,20007,"空指针异常"),
+    HTTP_CLIENT_ERROR(false,20008,"HTTP客户端错误")
+
     ;
 
     // 响应是否成功

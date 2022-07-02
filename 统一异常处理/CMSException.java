41606035180158df8c52f4868aecf682f4356e68
acoiasia <105906390+acoiasia@users.noreply.github.com>
2022-07-02 12:24:01 +0800

    Create CMSException.java

---
+++ 统一异常处理/CMSException.java
@@ -0,0 +1,26 @@
+import lombok.Data;
+
+/**
+ * @author 李永强
+ * @date 2022/7/2 12:22
+ * 自定义全局异常类
+ */
+@Data
+public class CMSException extends RuntimeException {
+    private Integer code;
+
+    public CMSException(Integer code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public CMSException(ResultCodeEnum resultCodeEnum) {
+        super(resultCodeEnum.getMessage());
+        this.code = resultCodeEnum.getCode();
+    }
+
+    @Override
+    public String toString() {
+        return "CMSException{" + "code=" + code + ", message=" + this.getMessage() + '}';
+    }
+}
